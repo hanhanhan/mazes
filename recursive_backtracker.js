5f487019a6dc21b87448382d50bc7ffea483d4e1
LearningNerd <learningnerd@gmail.com>
2017-01-25 11:48:30 -0800

    Liz's version 1, redraws whole maze each iteration

--- recursive_backtracker.js
+++ recursive_backtracker.js
@@ -1,182 +1,198 @@
-var canvas = document.getElementById('recursiveBacktracker')
-//recursive backtracker context
-var ctx = canvas.getContext('2d')
+var canvas = document.getElementById("mycanvas");
+var ctx = canvas.getContext("2d");
+var rows = 8;
+var cols = 8;
+var boxWidth = 50;
+var boxHeight = 50;
+var boxMargin = 4;
+var grid, stack, currentCell;
 
-var canvasWidth = 600
-var canvasHeight = 600
-var tileSize = 60
+// DOM elements and event listner
+var nextButton = document.getElementById('nextbutton');
+var restartButton = document.getElementById('restart');
+nextButton.addEventListener('click', drawNextStep);
+restartButton.addEventListener('click', initializeMazeGenerator);
 
-var interval = 500
-var drawTime = Date.now()
-var elapsed
-
-canvas.width = canvasWidth
-canvas.height = canvasHeight
-
-var rows = Math.floor(canvasWidth/tileSize)
-var columns = Math.floor(canvasHeight/tileSize)
-
-var grid = []
-// var current
-
-function setupMazeArray(){
-
-    for(var row=0; row < rows; row++){
-        var colObjs = []
-        for(var column=0; column < columns; column++){
-            colObjs.push(new Cell(row, column))
-        }
-        grid.push(colObjs)
-    }
-    // current = grid[0][0]
+function Cell (col, row) {
+    this.row = row;
+    this.col = col;
+    this.visited = false;
+    this.walls = {'top': true, 'right': true, 'bottom': true, 'left': true};
 }
 
-function Cell(row, column){
-    this.row = row
-    this.column = column
-    this.walls = { 'up': true, 'down':true, 'left': true, 'right': true };
-    this.visited = false
+Cell.prototype.draw = function() {
+    var x = this.col * boxWidth;
+    var y = this.row * boxHeight;
 
-    this.checkNeighbors = function checkNeighbors() {
-        // good place for curryed function?
-        if(!(this.row === 0)){
-            var left = grid[this.column][this.row - 1]
-        }
-        if(!(this.row === rows - 1)){
-            var right = grid[this.column][this.row + 1]
-        }
-        if(!(this.column === 0)){
-            var up = grid[this.column - 1][this.row]
-        }
-        if(!(this.column === columns - 1)){
-            var down = grid[this.column + 1][this.row]
-        }
+    // Highlight if it's the currentCell
+    if ( currentCell && this.row === currentCell.row && this.col === currentCell.col ) {
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.col * (boxWidth), this.row * (boxHeight), boxWidth, boxHeight);
+    } else if (this.visited) {
+        // otherwise, use these styles for visited cells:
+        ctx.fillStyle = 'orange';
+        ctx.strokeStyle = '#555';
+        ctx.fillRect(this.col * (boxWidth), this.row * (boxHeight), boxWidth, boxHeight);
+    } else {
+        // or these styles for unvisited cells:
+        ctx.fillStyle = '#fff';
+        ctx.strokeStyle = '#eee';
+    }
 
-        var neighbors = [left, right, up, down].filter(neighbor =>
-            neighbor && !neighbor.visited)
-        // console.log(neighbors)
+    ctx.lineWidth = boxMargin;
+    ctx.lineCap = 'square';
 
-        var nextIndex = Math.floor(Math.random() * neighbors.length)
+    // Draw each wall as individual lines
+    ctx.beginPath();
 
-        if(neighbors.length > 0){
-            return neighbors[nextIndex];
-        } else {
-            return false;
+    if (this.walls.top) {
+        ctx.moveTo(x, y);
+        ctx.lineTo(x + boxWidth, y);
+    }
+
+    if (this.walls.right) {
+        ctx.moveTo(x + boxWidth, y);
+        ctx.lineTo(x + boxWidth , y + boxHeight);
+    }
+
+    if (this.walls.bottom) {
+        ctx.moveTo(x, y + boxHeight);
+        ctx.lineTo(x + boxWidth, y + boxHeight);
+    }
+
+    if (this.walls.left) {
+        ctx.moveTo(x, y);
+        ctx.lineTo(x, y + boxHeight);
+    }
+
+    ctx.stroke();
+};
+
+Cell.prototype.checkRandomNeighbor = function() {
+    var neighbors = [grid[ getGridIndex(this.row, this.col - 1) ],
+    grid[ getGridIndex(this.row + 1, this.col) ],
+    grid[ getGridIndex(this.row, this.col + 1) ],
+    grid[ getGridIndex(this.row - 1, this.col) ]
+];
+
+var unvisitedNeighbors = neighbors.filter( function(neighbor) {return (neighbor && !neighbor.visited);} );
+
+if (unvisitedNeighbors.length > 0) {
+    return unvisitedNeighbors[ getRandomInt(0, unvisitedNeighbors.length - 1) ];
+}
+};
+
+function drawNextStep() {
+    // If there's an unvisited neighbor....
+    var nextCell = currentCell.checkRandomNeighbor();
+    if (nextCell) {
+        // push visited cell to the stack on each round:
+        stack.push(currentCell);
+
+        console.log('found a neighbor! pushed to stack:');
+        console.log(stack);
+
+        // remove cell walls accordingly and redraw
+        removeWalls(currentCell, nextCell);
+        //currentCell.draw();
+        //nextCell.draw();
+
+        // set currentCell for the next round and draw it:
+        currentCell = nextCell;
+
+        // mark next cell as visited
+        nextCell.visited = true;
+
+    } else if (stack.length > 0) {
+
+        // redraw the currentCell
+        // currentCell.draw();
+
+        // if we ran out of neighbors, revisit the last visited cell and start over!
+        currentCell = stack.pop();
+
+        console.log('popped one off the stack, backtracking!');
+        console.log(stack);
+    } else {
+        // if no more neighbors and the stack is empty, we're done! show reset button!
+        restartButton.style.display = 'block';
+    }
+
+    // LAZY SOLUTION FOR NOW: just redraw the entire grid each time
+    grid.forEach(function(cell){ cell.draw();});
+
+}
+
+function removeWalls(firstCell, secondCell) {
+    var neighborXSide = firstCell.col - secondCell.col;
+    var neighborYSide = firstCell.row - secondCell.row;
+
+    if (neighborXSide === 1) {
+        firstCell.walls.left = false;
+        secondCell.walls.right = false;
+    } else if (neighborXSide === -1) {
+        firstCell.walls.right = false;
+        secondCell.walls.left = false;
+    }
+
+    if (neighborYSide === 1) {
+        firstCell.walls.top = false;
+        secondCell.walls.bottom = false;
+    } else if (neighborYSide === -1) {
+        firstCell.walls.bottom = false;
+        secondCell.walls.top = false;
+    }
+
+}
+
+// * * * * * * * * * * INITIALIZATION * * * * * * * * * * *
+function initializeMazeGenerator() {
+    // clear canvas!
+    ctx.clearRect(0, 0, 800, 800);
+
+    // Reset grid, stack, and current cell!
+    grid = [];
+    stack = [];
+    currentCell = undefined;
+
+    // Create the grid of cells and draw each one
+    for (var row = 0; row < rows; row++) {
+        for (var col = 0; col < cols; col++) {
+            var newCell = new Cell(row, col);
+            newCell.draw();
+            grid.push(newCell);
         }
     }
 
-    this.checkVisited = function checkVisited(tile){
-        if(tile && !tile.visited){
-            this.neighbors.push(tile)
-        }
-    }
+    // initialize currentCell to start at top left corner
+    currentCell = grid[0];
 
-    this.next = function next(){
-
-    }
-
-    this.drawTile = function drawTile(color){
-
-        var x = this.column * tileSize
-        var y = this.row * tileSize
-
-        ctx.fillStyle = color
-        ctx.fillRect(x, y, x + tileSize, y + tileSize)
+    // draw first step
+    currentCell.visited = true;
+    currentCell.draw();
 
 
+    console.log(grid);
 
-        // ctx.fill()
-        // ctx.stroke()
+
+    // DOM stuff:
+    restartButton.style.display = 'none';
+}
+
+initializeMazeGenerator();
+
+
+// * * * * * * * * * * HELPER FUNCTIONS * * * * * * * * * * *
+function getGridIndex(row, col) {
+    if (row < 0 || col < 0 || row > cols - 1 || col > rows - 1) {
+        return -1;
+    }  else {
+        return row + col * cols;
     }
 }
 
-function removeWalls(currentTile, nextTile){
-    var side = currentTile.column - nextTile.column
-    var upDown = currentTile.row - nextTile.row
-
-    switch(side){
-        case 1:
-            currentTile.walls.left = false;
-            nextTile.walls.right = false;
-            break;
-        case -1:
-            currentTile.walls.right = false;
-            nextTile.walls.left = false;
-            break;
-        }
-
-    switch(upDown){
-        case 1:
-            currentTile.walls.top = false;
-            nextTile.walls.bottom = false;
-            break;
-        case -1:
-            currentTile.walls.bottom = false;
-            nextTile.walls.top = false;
-            break;
-
-    }
-}
-
-
-
-setupMazeArray()
-
-function draw(){
-  // draw whole array
-  for(var row=0; row < rows; row++){
-      for(var column=0; column < columns; column++){
-
-          ctx.clearRect(0, 0, canvasWidth, canvasHeight)
-
-          var x = grid[row][column].row * tileSize
-          var y = grid[row][column].column * tileSize
-
-          ctx.rect(x, y, x + tileSize, y + tileSize)
-
-          if (grid[row][column].visited === true){
-              ctx.fillStyle = 'hsla(180, 90%, 80%, 0.8)'
-          } else {
-              ctx.fillStyle = 'gray'
-          }
-
-          ctx.strokeStyle = 'white'
-          ctx.fill()
-          ctx.stroke()
-      }
-  }
-}
-
-draw()
-var current = grid[0][0]
-var stack = []
-crawlMaze()
-
-// https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_backtracker
-function crawlMaze(){
-
-  elapsed = Date.now() - drawTime
-
-    if(elapsed > interval){
-      var next = current.checkNeighbors()
-
-      if(next){
-          stack.push(current)
-          removeWalls(current, next)
-          next.visited = true
-
-      } else {
-          next = stack.pop()
-      }
-
-      // draw()
-
-      current.drawTile('red')
-      current = next
-      next.drawTile('white')
-
-
-      drawTime = Date.now()
-    }
-    window.requestAnimationFrame(crawlMaze)
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
